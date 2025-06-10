76450f225086370f94e19379dee4bbbeba1b28dc
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-06-10 09:47:03 +0700

    masih di milestone

--- app/stimulasi/[milestone_id]/page.tsx
+++ app/stimulasi/[milestone_id]/page.tsx
@@ -6,34 +6,41 @@ import ClientStoryGenerator from '../../components/ClientStoryGenerator';
 import { milestone } from '../../milestone';
 import MilestoneCard from '../../components/MilestoneCard';
 
-const DetailedMilestonePage = async ({ params }: { params: { milestone_id: string } }) => {
-    const currentMilestone = milestone.find(
-        (item) => item.milestone_id === Number(params.milestone_id)
-    );
+interface PageProps {
+  params: {
+    milestone_id: string;
+  };
+}
 
-    if (!currentMilestone) {
-        notFound(); 
-    }
+const DetailedMilestonePage = async ({ params }: PageProps) => {
+  const currentMilestone = milestone.find(
+    (item) => item.milestone_id === Number(params.milestone_id)
+  );
 
-    return (
-        <div>
-            <NavbarLoggedin />
-            <HeaderStimulasi />
-            <div className='mx-5 py-8 px-4'>
-                <div className='flex gap-x-10 items-center justify-center mb-10'>
-                    <MilestoneCard milestone={currentMilestone} />
-                    
-                    <div className='flex flex-col gap-y-4 w-1/2'>
-                        <h2 className='font-semibold bg-transparent'>Ulangi {currentMilestone.title} Saat Berinteraksi</h2>
-                        <p>akjnckjsaadas</p>
-                    </div>
-                </div>
+  if (!currentMilestone) {
+    notFound(); 
+  }
 
-                <ClientStoryGenerator milestone={currentMilestone} />
-            </div>
-            <Footer />
+  return (
+    <div>
+      <NavbarLoggedin />
+      <HeaderStimulasi />
+      <div className='mx-5 py-8 px-4'>
+        <div className='flex gap-x-10 items-center justify-center mb-10'>
+          <MilestoneCard milestone={currentMilestone} />
+          <div className='flex flex-col gap-y-4 w-1/2'>
+            <h2 className='font-semibold bg-transparent'>
+              Ulangi {currentMilestone.title} Saat Berinteraksi
+            </h2>
+            <p>akjnckjsaadas</p>
+          </div>
         </div>
-    );
+
+        <ClientStoryGenerator milestone={currentMilestone} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default DetailedMilestonePage;
