053782c5c70abb2fd857c33f0b882ad5845f7340
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-06-10 15:55:50 +0700

    middleware fixation

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,23 +2,28 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    const res = NextResponse.next();
-    const supabase = createMiddlewareClient({ req, res });
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
 
-    console.log(session); 
-    
-    if(!session) {
-        return NextResponse.rewrite(new URL('/login', req.url));
-    }
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-    return res;
+  // Redirect ke '/' kalau user udah login tapi buka /login
+  if (session && req.nextUrl.pathname === "/Login") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  // Kalau belum login, arahkan ke /login
+  if (!session) {
+    return NextResponse.redirect(new URL("/Login", req.url));
+  }
+
+  return res;
 }
 
 export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
-    ]
-}
+  matcher: [
+    "/((?!^api/|^_next/|favicon.ico|^Login$|^Signup$).*)",
+  ],
+};
