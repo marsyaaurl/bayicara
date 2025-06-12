d5094824e12f72298b4aad08bba328f9ebc037f1
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-06-12 17:02:08 +0700

    header

--- app/components/HeaderStimulasi.tsx
+++ app/components/HeaderStimulasi.tsx
@@ -4,7 +4,8 @@ const HeaderStimulasi = () => {
             <div className="flex flex-col bg-transparent items-center justify-center text-background z-50 px-4 text-center max-w-2xl">
             <h1 className="text-3xl font-bold mb-1 bg-transparent">Stimulasi Bicara</h1>
             <p className="text-md font-normal bg-transparent">
-                tes assalamualaikum lorem ipsum
+                Bantu perkembangan bicara anak sejak dini melalui aktivitas menyenangkan dan penuh interaksi. 
+                Temukan cara kreatif untuk merangsang kemampuan komunikasinya setiap hari.
             </p>
             </div>
         </div>
